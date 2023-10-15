066caffb398b83428aa837f4ec4fbf35bac1418c
Roman <roman.zuchowski@gmail.com>
2023-10-15 16:43:06 +0200

    Initial Commit

---
+++ chargeclaus/chargeclausApp.swift
@@ -0,0 +1,32 @@
+//
+//  chargeclausApp.swift
+//  chargeclaus
+//
+//  Created by Roman Zuchowski on 15.10.23.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct chargeclausApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
