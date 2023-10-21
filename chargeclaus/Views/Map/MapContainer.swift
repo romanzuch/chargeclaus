614f21fc00cbdefdd2b6dcc2285e84a52ff87155
Roman <roman.zuchowski@gmail.com>
2023-10-21 16:51:03 +0200

    this configures the uikit map

--- chargeclaus/Views/Map/MapContainer.swift
+++ chargeclaus/Views/Map/MapContainer.swift
@@ -1,5 +1,5 @@
 //
-//  MapViewModel.swift
+//  MapContainer.swift
 //  chargeclaus
 //
 //  Created by Roman Zuchowski on 20.10.23.
@@ -9,7 +9,20 @@ import SwiftUI
 import MapKit
 
 struct MapContainer: UIViewRepresentable {
-    func makeUIView(context: Context) -> MapView {
-        return MapView()
+    
+    func makeUIView(context: Context) -> MKMapView {
+        let mapView: MKMapView = MKMapView()
+        mapView.showsUserLocation = true
+        mapView.showsCompass = true
+        mapView.showsUserTrackingButton = true
+        return mapView
+    }
+    
+    func updateUIView(_ uiView: MKMapView, context: Context) {
+        uiView.delegate = context.coordinator
+    }
+    
+    func makeCoordinator() -> MapCoordinator {
+        return MapCoordinator(self)
     }
 }
