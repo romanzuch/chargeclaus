523b29dd5124b974385ff7d3d814793cf13812ce
Roman <roman.zuchowski@gmail.com>
2023-10-21 16:49:41 +0200

    this adds a custom error type

--- chargeclaus/Models/CustomError.swift
+++ chargeclaus/Models/CustomError.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+
+enum ChargeclausError: Error {
+    case firestoreError
+    case decodingError
+    case unexpected(code: Int)
+}
+
+extension ChargeclausError: LocalizedError {
+    public var errorDescription: String? {
+        switch self {
+        case .firestoreError:
+            return NSLocalizedString(
+                "An error occured while retreiving the data from Firebase.",
+                comment: "Firebase connection error"
+            )
+        case .decodingError:
+            return NSLocalizedString(
+                "The retreived data could not be decoded.",
+                comment: "Decoding Error"
+            )
+        case .unexpected(_):
+            return NSLocalizedString(
+                "An unexpected error occurred.",
+                comment: "Unexpected Error"
+            )
+        }
+    }
+}
