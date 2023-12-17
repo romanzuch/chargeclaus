4541d8c0f571afdf2c16c1a3ccb420f851c913d5
Roman <roman.zuchowski@gmail.com>
2023-12-17 22:50:58 +0100

    this will add the basic details

--- chargeclaus/Views/Details/LocationDetails.swift
+++ chargeclaus/Views/Details/LocationDetails.swift
@@ -10,12 +10,33 @@ import SwiftUI
 struct LocationDetails: View {
     
     let location: Location?
+    @StateObject private var vm: LocationDetailsViewModel = LocationDetailsViewModel()
     
     var body: some View {
-        if let location = location {
-            Text(location.address.getFullAddress())
+        if let location = location, let connections = location.connections {
+            VStack(alignment: .leading) {
+                vm.getNumberOfConnectionsText(for: connections)
+                    .fontWeight(.light)
+                    .font(.caption)
+                Text(location.address.getFullAddress())
+                    .fontWeight(.bold)
+                vm.getAvailableConnectionsText(for: connections)
+                HStack {
+                    Text("\(connections[0].type.rawValue)")
+                        .font(.caption)
+                    Spacer()
+                    vm.getMaxPowerOutputText(for: connections)
+                        .font(.caption)
+                }
+                Spacer()
+            }
+            .padding()
         } else {
             Text("Keine Daten vorhanden.")
         }
     }
+}
+
+#Preview {
+    LocationDetails(location: Location.example)
 }
